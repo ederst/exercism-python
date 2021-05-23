c0302714115ce3ecdcfb883554eaf82c4e423509
ederst <stefan.prietl@bearingpoint.com>
2021-05-23 13:48:35 +0200

    WIP: prototype exercism cd with metadata parsing

---
+++ .hack/metadata-parser.py
@@ -0,0 +1,35 @@
+import docstring_parser
+import ast
+import yaml
+
+file_name = r"high-scores/high_scores.py"
+
+
+def parse_docstring_from_file(f):
+    return ast.get_docstring(ast.parse(''.join(f)))
+
+
+with open(file_name) as f:
+    doc_string = docstring_parser.parse(parse_docstring_from_file(f))
+
+print("The doc string:")
+print("Short description", doc_string.short_description)
+print("Long description", doc_string.long_description)
+
+# Note: possible do it via params
+print("Params:")
+for param in doc_string.params:
+    print(f"- {param.arg_name}: {param.description}")
+
+# Note:
+#   or do it via long description + yaml/json whatever
+#   ^ probably the better solution for it being a more general approach for other languages as well:
+#     rust/c/c++/whatever:
+#       /*
+#        *  Short description
+#        *
+#        *  <Yaml as long description meta whatever>
+#        */
+exercism_metadata = yaml.safe_load(doc_string.long_description)
+
+print(exercism_metadata)
--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,3 +1,22 @@
+"""
+Solution of 'high-scores' exercise in the 'python' track.
+
+exercism-cd:
+    exercise: high-scores
+    track: python
+    author: ederst
+    git-url: https://github.com/ederst/exercism-python.git
+    pull-request: https://github.com/ederst/exercism-python/pull/11
+    state: draft|submit|submitted|reviewed
+
+:param exercise: high-scores
+:param track: python
+:param state: draft
+"""
+
+import yaml
+
+
 def latest(scores: list[int]) -> int:
     return scores[-1]
 
@@ -8,3 +27,4 @@ def personal_best(scores: list[int]) -> int:
 
 def personal_top_three(scores: list[int]) -> list[int]:
     return sorted(scores, reverse=True)[:3]
+
