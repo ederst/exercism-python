b6c168648159052a2fad1e847c3b11473897c676
ederst <stefan.prietl@bearingpoint.com>
2021-05-23 12:51:51 +0200

    Add additional 'top three' solution to 'high-scores' exercise

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -8,3 +8,11 @@ def personal_best(scores: list[int]) -> int:
 
 def personal_top_three(scores: list[int]) -> list[int]:
     return sorted(scores, reverse=True)[:3]
+
+
+def personal_top_three_slice_magic(scores: list[int]) -> list[int]:
+    """
+    This uses step -1 and a negative index instead of reverse sorting the list.
+    """
+
+    return sorted(scores)[:-4:-1]
