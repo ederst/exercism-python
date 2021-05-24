3743281750ba8e063fbca8d0fe800993a8d5bfa4
ederst <stefan.prietl@bearingpoint.com>
2021-05-25 01:05:16 +0200

    Add final sulution for the 'phone-number' exercise
    This is what I came up with after some research.

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -3,28 +3,60 @@ import re
 
 class PhoneNumber:
 
-    number: str
+    # Note: Use 'private' members and use @property to expose them, to make it as hard as possible [1] to set an
+    # unvalidated value from the outside.
+    #
+    # So this will throw and error:
+    #
+    # ```python
+    # pn = PhoneNumber("1 234 567 8900")
+    # pn.area_code = "Oh no!"
+    # ```
+    #
+    # But this will still work:
+    #
+    # ```python
+    # pn = PhoneNumber("1 234 567 8900")
+    # pn._PhoneNumber__area_code = "Ha ha!"
+    # ```
+    #
+    # [1]: https://docs.python.org/3/tutorial/classes.html#private-variables
+    __area_code: str
+    __exchange_code: str
+    __subscriber_number: str
 
-    area_code: str
-    exchange_code: str
-    subscriber_number: str
+    # Inspiration: https://exercism.io/tracks/python/exercises/phone-number/solutions/5d851e650c05492e8bcfbe80e7ddc308
+    __pattern = re.compile(r"""
+        ^1?             # (optional) country code
+        ([2-9]\d{2})    # area code
+        ([2-9]\d{2})    # exchange code
+        (\d{4})$        # subscriber number
+    """, re.VERBOSE)
 
     def __init__(self, number: str):
-        number = re.sub(r"(^[\+]?1|\D+)", "", number)
+        # Cleanup number to make for easier parsing
+        number = re.sub(r"\D+", "", number)
 
-        if len(number) != 10:
-            raise ValueError("Invalid number")
+        try:
+            self.__area_code, self.__exchange_code, self.__subscriber_number = self.__pattern.search(number).groups()
+        except AttributeError:
+            raise ValueError("Provided an invalid number")
 
-        if int(number[0]) <= 1:
-            raise ValueError("Invalid area code")
+    @property
+    def area_code(self):
+        return self.__area_code
 
-        if int(number[3]) <= 1:
-            raise ValueError("Invalid exchange code")
+    @property
+    def exchange_code(self):
+        return self.__exchange_code
 
-        self.number = number
-        self.area_code = number[:3]
-        self.exchange_code = number[3:6]
-        self.subscriber_number = number[6:]
+    @property
+    def subscriber_number(self):
+        return self.__subscriber_number
+
+    @property
+    def number(self):
+        return f"{self.area_code}{self.exchange_code}{self.subscriber_number}"
 
     def pretty(self):
         return f"({self.area_code})-{self.exchange_code}-{self.subscriber_number}"
