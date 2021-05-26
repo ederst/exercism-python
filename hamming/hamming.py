b00679b44b89e774fe480707f4267a97f18ca198
ederst <stefan.prietl@bearingpoint.com>
2021-05-26 18:39:51 +0200

    Add solution for 'hamming' using zip and sum

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -2,8 +2,4 @@ def distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) != len(strand_b):
         raise ValueError("Invalid input.")
 
-    distance = 0
-    for i, c in enumerate(strand_a):
-        if c != strand_b[i]:
-            distance += 1
-    return distance
+    return sum(x != y for x, y in zip(strand_a, strand_b))
