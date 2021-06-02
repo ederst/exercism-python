e489e750446fada4ecb716041305088434a07052
ederst <stefan.prietl@bearingpoint.com>
2021-06-02 13:29:38 +0200

    Solve 'bank-account' with a state machine

--- bank-account/bank_account.py
+++ bank-account/bank_account.py
@@ -1,53 +1,90 @@
 from threading import Lock
 
 
-class BankAccount:
+class State(object):
 
-    def _reset(self):
-        self._opened = False
-        self._balance = 0
-        self._lock = Lock()
-
-    def __init__(self):
-        self._reset()
-
-    def get_balance(self):
-        with self._lock:
-            if not self._opened:
-                raise ValueError("Account is closed.")
-
-            return self._balance
-
-    def open(self):
-        if self._opened:
-            raise ValueError("Account is already opened.")
-
-        self._opened = True
-
-    def deposit(self, amount):
-        with self._lock:
-            if not self._opened:
-                raise ValueError("Account is closed.")
-
-            if amount < 0:
-                raise ValueError("Amount cannot be negative.")
-
-            self._balance += amount
-
-    def withdraw(self, amount):
-        with self._lock:
-            if not self._opened:
-                raise ValueError("Account is closed.")
-
-            if amount < 0:
-                raise ValueError("Amount cannot be negative.")
-
-            if amount > self._balance:
-                raise ValueError("Cannot withdraw more than deposited.")
-
-            self._balance -= amount
+    def get_balance(self, account) -> int:
+        raise NotImplementedError
 
     def close(self):
-        if not self._opened:
-            raise ValueError("Account is already closed.")
-        self._reset()
+        raise NotImplementedError
+
+    def open(self):
+        raise NotImplementedError
+
+    def deposit(self, account, amount):
+        raise NotImplementedError
+
+    def withdraw(self, account, amount):
+        raise NotImplementedError
+
+
+class Opened(State):
+
+    def get_balance(self, account) -> int:
+        return account._balance
+
+    def close(self) -> State:
+        return Closed()
+
+    def open(self):
+        raise ValueError("Account is open.")
+
+    def deposit(self, account, amount):
+        if amount <= 0:
+            raise ValueError("Invalid amount.")
+
+        with account._lock:
+            account._balance += amount
+
+    def withdraw(self, account, amount):
+        if not 0 < amount <= account._balance:
+            raise ValueError("invalid amount.")
+
+        with account._lock:
+            account._balance -= amount
+
+
+class Closed(State):
+
+    def _raise_error(self):
+        raise ValueError("Account is closed.")
+
+    def get_balance(self, account):
+        self._raise_error()
+
+    def close(self) -> State:
+        self._raise_error()
+
+    def open(self) -> State:
+        return Opened()
+
+    def deposit(self, account, amount):
+        self._raise_error()
+
+    def withdraw(self, account, amount):
+        self._raise_error()
+
+
+class BankAccount(object):
+
+    def __init__(self):
+        self._lock = Lock()
+        self._state = Closed()
+        self._balance = 0
+
+    def get_balance(self) -> int:
+        return self._state.get_balance(self)
+
+    def open(self):
+        self._state = self._state.open()
+
+    def deposit(self, amount):
+        self._state.deposit(self, amount)
+
+    def withdraw(self, amount):
+        self._state.withdraw(self, amount)
+
+    def close(self):
+        self._state = self._state.close()
+        self._balance = 0
