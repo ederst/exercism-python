a57458040883bbda43ff881b4ee365921a11b8de
ederst <stefan.prietl@bearingpoint.com>
2021-06-05 22:02:29 +0200

    Refine 'twelve-days' solution

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,4 +1,6 @@
-ordinals = [
+from typing import List
+
+ordinals: List[str] = [
     "first",
     "second",
     "third",
@@ -13,7 +15,7 @@ ordinals = [
     "twelfth",
 ]
 
-gifts = [
+gifts: List[str] = [
     "a Partridge in a Pear Tree",
     "two Turtle Doves, and",
     "three French Hens,",
@@ -29,8 +31,27 @@ gifts = [
 ]
 
 
-def recite(start_verse, end_verse):
+def require_valid_verse_range(func):
+    def require_valid_verse_range_func(start_verse: int, end_verse: int):
+        len_gifts = len(gifts)
+
+        assert end_verse >= start_verse, "End verse must be greater than or equal to start verse."
+        assert (
+            1 <= end_verse <= len_gifts and 1 <= start_verse <= len_gifts
+        ), f"Start and end verse must be a number between 1 and {len_gifts}"
+
+        return func(start_verse, end_verse)
+
+    return require_valid_verse_range_func
+
+
+@require_valid_verse_range
+def recite(start_verse: int, end_verse: int) -> List[str]:
+
+    # adjust start_verse to "real" beginning of lists in python, which is 0
+    start_verse -= 1
+
     return [
-        f"On the {ordinal} day of Christmas my true love gave to me: {' '.join(gifts[ordinals.index(ordinal)::-1])}."
-        for ordinal in ordinals[start_verse - 1:end_verse]
+        f"On the {ordinal} day of Christmas my true love gave to me: {' '.join(gifts[i+start_verse::-1])}."
+        for i, ordinal in enumerate(ordinals[start_verse:end_verse])
     ]
