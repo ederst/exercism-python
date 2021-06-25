98808bec7b3aa162060cf49c3e97d39681f4cdce
Stefan Prietl <ederst@users.noreply.github.com>
2021-06-25 18:17:12 +0200

    Parse words for 'word-count' exercise
    This parses the word, instead of getting rid of the superfluous chars.
    
    Thanks to @bobahop who suggested this easier method.

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,19 +1,8 @@
 from collections import Counter
-from string import ascii_lowercase, digits
 from typing import Dict
 import re
 
 
-_quotes = '"\''
-_exceptions = ascii_lowercase + digits + _quotes
-
-
 def count_words(sentence: str) -> Dict[str, int]:
-    # lowercase the sentence and substitute everything except letters, digits and quotes with a whitespace
-    sentence = re.sub(f'[^{_exceptions}]', ' ', sentence.casefold())
-
-    # create a list of words out of the sentence and strip quotes from words (ignoring the contractions)
-    words = [word.strip(_quotes) for word in sentence.split()]
-
-    # count the words and return it as dict
-    return dict(Counter(words))
+    # this exact solution was suggestec by the mentor (bobahop)
+    return Counter(re.findall(r"[a-z0-9]+(?:'[a-z]+)?", sentence.lower()))
