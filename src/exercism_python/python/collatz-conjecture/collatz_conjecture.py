c497e486a6ff0d206dccccbe7a4bf493b535a086
ederst <stefan.prietl@bearingpoint.com>
2024-12-05 10:53:35 +0100

    WIP: feat(collatz-conjecture): solve exercise with iteration and funcs

--- src/exercism_python/python/collatz-conjecture/collatz_conjecture.py
+++ src/exercism_python/python/collatz-conjecture/collatz_conjecture.py
@@ -1,11 +1,26 @@
+def _is_even(number: int) -> bool:
+    return number % 2 == 0
+
+
+def _get_next(number: int) -> int:
+    """Get the next number of the Collatz Conjecture.
+
+    :param number: int - the current number.
+    :return: int - if `number` is even, the `number` divided by 2, or otherwise "3 * `number` + 1".
+    """
+    if _is_even(number):
+        return number // 2
+
+    return 3 * number + 1
+
+
 def steps(number: int) -> int:
     if number <= 0:
         raise ValueError('Only positive integers are allowed')
 
     steps = 0
-
     while number > 1:
-        number = (3 * number + 1) if number % 2 else (number // 2)
+        number = _get_next(number)
         steps += 1
 
     return steps
