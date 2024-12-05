98af4237ceb7380f1396c4a65f20b0170fe1adcc
ederst <stefan.prietl@bearingpoint.com>
2024-12-05 10:48:01 +0100

    WIP: feat(collatz-conjecture): solve exercise with recursion

--- src/exercism_python/python/collatz-conjecture/collatz_conjecture.py
+++ src/exercism_python/python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
-def steps(number):
-    pass
+def _steps(number: int) -> int:
+    if number == 1:
+        return 0
+
+    if number % 2:
+        return 1 + _steps(3 * number + 1)
+
+    return 1 + _steps(number // 2)
+
+
+def steps(number: int) -> int:
+    if number <= 0:
+        raise ValueError('Only positive integers are allowed')
+
+    return _steps(number)
