ae9428e72fc6b15649e5b5863e1f47d0dc8cf47f
ederst <stefan.prietl@bearingpoint.com>
2024-12-05 10:52:27 +0100

    WIP: feat(collatz-conjecture): solve exercise with iteration and ternery operator

--- src/exercism_python/python/collatz-conjecture/collatz_conjecture.py
+++ src/exercism_python/python/collatz-conjecture/collatz_conjecture.py
@@ -1,15 +1,11 @@
-def _steps(number: int) -> int:
-    if number == 1:
-        return 0
-
-    if number % 2:
-        return 1 + _steps(3 * number + 1)
-
-    return 1 + _steps(number // 2)
-
-
 def steps(number: int) -> int:
     if number <= 0:
         raise ValueError('Only positive integers are allowed')
 
-    return _steps(number)
+    steps = 0
+
+    while number > 1:
+        number = (3 * number + 1) if number % 2 else (number // 2)
+        steps += 1
+
+    return steps
